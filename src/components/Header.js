bf7927d159d0e121f2c97b7748611df073aa011a
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-03-24 18:28:19 +0200

    header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,38 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useCallback, useState } from 'react'
 import styles from '../style/Header.module.scss'
 import Logo from './Logo'
+import { useSpring, animated, config } from 'react-spring'
 
-const Header = class extends React.Component {
-  render() {
-    return (
-      <nav className={styles.Header}>
-        <div className={styles.HeaderContent}>
-          <Link to="/" title="Logo" className={styles.LogoLink}>
-            <Logo />
-          </Link>
-        </div>
-      </nav>
-    )
-  }
+const transformNav = left => `translate(${left}%)`
+
+const Header = () => {
+  const [navVisible, toggleNav] = useState(false)
+
+  const onLogoClick = useCallback(() => {
+    toggleNav(!navVisible)
+  }, [navVisible])
+
+  const navStyle = useSpring({
+    config: { mass: 0.2, tension: 200, friction: 15 },
+    opacity: navVisible ? 1 : 0,
+    left: navVisible ? 0 : -100,
+  })
+
+  return (
+    <nav className={styles.Header}>
+      <div className={styles.HeaderContent}>
+        <button onClick={onLogoClick} className={styles.LogoLink}>
+          <Logo />
+        </button>
+      </div>
+      <animated.nav
+        style={{
+          opacity: navStyle.opacity,
+          transform: navStyle.left.interpolate(transformNav),
+        }}
+        className={styles.HeaderNav}
+      />
+    </nav>
+  )
 }
-
 export default Header
