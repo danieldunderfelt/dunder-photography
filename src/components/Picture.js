9630d2f4124bc53c851f31af01fa2036e125506f
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-03-25 18:56:08 +0200

    Show view button for mobile devices.

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,33 +1,31 @@
 import styles from '../style/Picture.module.scss'
 import React, { useState, useCallback } from 'react'
-import { Link } from 'gatsby'
+import { Link, navigate } from 'gatsby'
 import PictureTags from './PictureTags'
 import { isTouchDevice } from '../util/isTouchDevice'
 
+const isTouch = isTouchDevice()
+
 const Picture = ({ picture, overlay, linkTo, showInfo = true }) => {
   const { title, file, alt, description, tags } = picture || {}
-  const LinkComponent = linkTo ? Link : 'div'
 
   const [active, setActive] = useState(false)
-  const onClick = useCallback(() => setActive(!active), [active])
-
-  const onClickLink = useCallback(
-    e => {
-      if (!active && isTouchDevice()) {
-        e.preventDefault()
-      }
-    },
-    [active]
-  )
+  const onClick = useCallback(() => {
+    if (!isTouch && linkTo) {
+      navigate(linkTo)
+    } else {
+      setActive(!active)
+    }
+  }, [active])
 
   const onMouseOver = useCallback(() => {
-    if (!isTouchDevice()) {
+    if (!isTouch) {
       setActive(true)
     }
   }, [])
 
   const onMouseOut = useCallback(() => {
-    if (!isTouchDevice()) {
+    if (!isTouch) {
       setActive(false)
     }
   }, [])
@@ -38,15 +36,22 @@ const Picture = ({ picture, overlay, linkTo, showInfo = true }) => {
       onMouseLeave={onMouseOut}
       onClick={onClick}
       className={`${styles.PictureContainer} ${active ? styles.active : ''}`}>
-      <LinkComponent onClick={onClickLink} to={linkTo}>
-        <img className={styles.Picture} alt={alt} src={file} />
-        {showInfo && !overlay && (
-          <div className={styles.PictureInfo}>
-            <h2 className={styles.PictureTitle}>{title}</h2>
-            <div className={styles.Description}>{description}</div>
-          </div>
-        )}
-      </LinkComponent>
+      <img className={styles.Picture} alt={alt} src={file} />
+      {showInfo && !overlay && (
+        <div className={styles.PictureInfo}>
+          <h2 className={styles.PictureTitle}>{title}</h2>
+          <div className={styles.Description}>{description}</div>
+          {linkTo && (
+            <Link
+              className={`${styles.PictureLink} ${
+                !isTouch ? styles.Deemphasize : ''
+              }`}
+              to={linkTo}>
+              View
+            </Link>
+          )}
+        </div>
+      )}
       {overlay && (
         <div className={styles.VisibleOverlay}>
           <h2 className={styles.OverlayTitle}>{overlay}</h2>
